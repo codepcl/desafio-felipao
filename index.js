7f1753ef6805bd243cfa6d270bf7138494a547e0
Pierr Daniel Chino Lurita <pchinol11@gmail.com>
2024-04-30 23:35:11 -0300

    feat: uncommented

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+function calcRanking(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else if (vitorias >= 101) {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`;
+}
+
+let resultado = calcRanking(85, 30);
+console.log(resultado);
+
+/*
 let nome, nivel, xp;
 
 nome = "Boss";
@@ -32,3 +59,5 @@ while (xp) {
 }
 
 console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+
+*/
